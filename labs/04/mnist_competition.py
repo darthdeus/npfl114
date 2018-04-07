30eeb56f994031a1ad1a50e2f4cd88258e2b735d
Jakub Arnold <darthdeus@gmail.com>
2018-04-07 22:48:10 +0200

    Final mnist comp

--- labs/04/mnist_competition.py
+++ labs/04/mnist_competition.py
@@ -2,6 +2,7 @@
 import numpy as np
 import tensorflow as tf
 
+
 class Network:
     WIDTH = 28
     HEIGHT = 28
@@ -11,17 +12,18 @@ class Network:
         # Create an empty graph and a session
         graph = tf.Graph()
         graph.seed = seed
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.6)
-        self.session = tf.Session(graph = graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
-                                                                       intra_op_parallelism_threads=threads,
-                                                                       gpu_options=gpu_options))
+        gpu_options = tf.GPUOptions(allow_growth=True)
+
+        self.session = tf.Session(graph=graph, config=tf.ConfigProto(inter_op_parallelism_threads=threads,
+                                                                     intra_op_parallelism_threads=threads,
+                                                                     gpu_options=gpu_options))
 
     def construct(self, args):
         with self.session.graph.as_default():
             # Inputs
             self.images = tf.placeholder(tf.float32, [None, self.WIDTH, self.HEIGHT, 1], name="images")
             self.labels = tf.placeholder(tf.int64, [None], name="labels")
-            self.is_training = tf.placeholder(tf.bool, [], name="is_training")
+            self.is_training = tf.placeholder_with_default(False, [], name="is_training")
 
             # Computation
             # TODO: Add layers described in the args.cnn. Layers are separated by a comma and
@@ -77,12 +79,9 @@ class Network:
                                                units=int(layer_args[1]),
                                                activation=tf.nn.relu)
 
-
                     features = tf.layers.dropout(features,
-                                                0.5,
-                                                training=self.is_training)
-
-
+                                                 0.5,
+                                                 training=self.is_training)
 
             output_layer = tf.layers.dense(features, self.LABELS, activation=None, name="output_layer")
             self.predictions = tf.argmax(output_layer, axis=1)
@@ -92,12 +91,15 @@ class Network:
             global_step = tf.train.create_global_step()
 
             with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
-                self.training = tf.train.AdamOptimizer(learning_rate=0.0003).minimize(loss, global_step=global_step, name="training")
-
+                self.training = tf.train.AdamOptimizer(learning_rate=0.0003).minimize(loss,
+                                                                                      global_step=global_step,
+                                                                                      name="training")
 
             # Summaries
             self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.labels, self.predictions), tf.float32))
+
             summary_writer = tf.contrib.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
+
             self.summaries = {}
             with summary_writer.as_default(), tf.contrib.summary.record_summaries_every_n_global_steps(100):
                 self.summaries["train"] = [tf.contrib.summary.scalar("train/loss", loss),
@@ -123,17 +125,11 @@ class Network:
         accuracy, _ = self.session.run([self.accuracy, self.summaries[dataset]], {
             self.images: images,
             self.labels: labels,
-            self.is_training: False
         })
         return accuracy
 
     def predict(self, images):
-        return self.session.run([self.predictions], {
-            self.images: images,
-            self.is_training: False
-            })
-
-
+        return self.session.run(self.predictions, {self.images: images})
 
 
 if __name__ == "__main__":
@@ -148,12 +144,11 @@ if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", default=128, type=int, help="Batch size.")
-    parser.add_argument("--cnn", default=None, type=str, help="Description of the CNN architecture.")
-    parser.add_argument("--epochs", default=15, type=int, help="Number of epochs.")
+    parser.add_argument("--cnn", default="CB-128-3-1-same,M-3-2,CB-128-3-1-same,CB-64-3-1-same,M-2-1,F,R-1024", type=str,
+                        help="Description of the CNN architecture.")
+    parser.add_argument("--epochs", default=20, type=int, help="Number of epochs.")
     parser.add_argument("--threads", default=12, type=int, help="Maximum number of threads to use.")
     args = parser.parse_args()
-
-    args.cnn = "CB-128-3-1-same,CB-64-3-1-same,M-2-1,F,R-1024"
 
     # Create logdir name
     args.logdir = "logs/{}-{}-{}".format(
@@ -161,11 +156,14 @@ if __name__ == "__main__":
         datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
         ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", key), value) for key, value in sorted(vars(args).items())))
     )
-    if not os.path.exists("logs"): os.mkdir("logs") # TF 1.6 will do this by itself
+    if not os.path.exists("logs"): os.mkdir("logs")  # TF 1.6 will do this by itself
 
     # Load the data
     from tensorflow.examples.tutorials import mnist
-    mnist = mnist.input_data.read_data_sets("mnist-gan", reshape=False, seed=42,
+
+    mnist = mnist.input_data.read_data_sets("mnist-gan",
+                                            reshape=False,
+                                            seed=42,
                                             source_url="https://ufal.mff.cuni.cz/~straka/courses/npfl114/1718/mnist-gan/")
 
     # Construct the network
@@ -180,12 +178,17 @@ if __name__ == "__main__":
 
         network.evaluate("dev", mnist.validation.images, mnist.validation.labels)
 
-    labels = []
+    label_groups = []
 
-    while mnist.test.epochs_completed == 0:
-        test_images, _ = mnist.test.next_batch(args.batch_size)
-        test_labels = network.predict(test_images)
+    bs = 2048
 
-    with open("mnist_competition_test.txt", "w") as test_file:
-        for label in test_labels:
-            print(label, file=test_file)
+    test_imgs = mnist.test.images
+    N = test_imgs.shape[0]
+
+    for idx in np.split(np.arange(N), N // bs):
+        test_batch = test_imgs[idx]
+        label_groups.append(network.predict(test_batch))
+
+    test_labels = np.concatenate(label_groups)
+
+    np.savetxt("mnist_competition_test.txt", test_labels, fmt="%d", delimiter="\n")
