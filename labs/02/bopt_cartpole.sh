cb1474c40ab40715383b6df4a1d9418296b6f78b
Jakub Arnold <darthdeus@gmail.com>
2019-03-22 03:47:21 +0100

    Add requirements.txt + make bopt_cartpole.sh use paths relative to the script

--- labs/02/bopt_cartpole.sh
+++ labs/02/bopt_cartpole.sh
@@ -8,6 +8,8 @@ cd $DIR
 # $HOME/personal_work_ms/npfl114/labs/.venv/bin/python gym_cartpole.py $@ 2>&1 >>log_cartpole.txt
 # $HOME/personal_work_ms/npfl114/labs/.venv/bin/python gym_cartpole_evaluate.py gym_cartpole_model$JOB_ID.h5
 
-../.cpu-venv/bin/python gym_cartpole.py $@ 2>&1 >>log_cartpole.txt
-../.cpu-venv/bin/python gym_cartpole_evaluate.py 2>>log_cartpole.txt
+export JOB_ID=${JOB_ID:-$$}
+
+$DIR/../.venv/bin/python $DIR/gym_cartpole.py $@ 2>&1 >>log_cartpole.txt
+$DIR/../.venv/bin/python $DIR/gym_cartpole_evaluate.py 2>>log_cartpole.txt
 
