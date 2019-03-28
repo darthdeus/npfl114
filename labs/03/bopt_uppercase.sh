8e6bc6d7e20376b372318582a227f8c5fea9cf0e
Jakub Arnold <darthdeus@gmail.com>
2019-03-28 03:30:23 +0100

    Print output fnames for uppercase

--- labs/03/bopt_uppercase.sh
+++ labs/03/bopt_uppercase.sh
@@ -11,5 +11,11 @@ mkdir -p tmp
 test_out_fname=tmp/uppercase_test_out$JOB_ID.txt
 dev_out_fname=tmp/uppercase_dev_out$JOB_ID.txt
 
+echo "Outputs are:
+
+dev:  $dev_out_fname
+test: $test_out_fname
+"
+
 $DIR/../.venv/bin/python $DIR/uppercase.py --dev_out_fname=$dev_out_fname --test_out_fname=$test_out_fname $@ 2>&1
 $DIR/../.venv/bin/python $DIR/uppercase_eval.py $dev_out_fname uppercase_data_dev.txt 2>&1
