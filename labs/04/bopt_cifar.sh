d526d1e87499ec8c6175d5c9d78ffcd647412ff3
Jakub Arnold <darthdeus@gmail.com>
2019-04-07 14:49:53 +0200

    Add bopt_cifar.sh

---
+++ labs/04/bopt_cifar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+
+export JOB_ID=${JOB_ID:-$$}
+
+# mkdir -p tmp
+# test_out_fname=tmp/uppercase_test_out$JOB_ID.txt
+# dev_out_fname=tmp/uppercase_dev_out$JOB_ID.txt
+#
+# echo "Outputs are:
+#
+# dev:  $dev_out_fname
+# test: $test_out_fname
+# "
+
+# $DIR/../.venv/bin/python $DIR/uppercase.py --dev_out_fname=$dev_out_fname --test_out_fname=$test_out_fname $@ 2>&1
+# $DIR/../.venv/bin/python $DIR/uppercase_eval.py $dev_out_fname uppercase_data_dev.txt 2>&1
+
+source $HOME/.profile
+
+$DIR/.venv/bin/python cifar_competition_bopt.py $@ # 2>&1
